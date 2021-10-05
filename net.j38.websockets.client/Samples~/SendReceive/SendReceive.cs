cf0cab3e81f9be54743cfa79fbeb6a2949439b89
Scott Garner <scott@j38.net>
2021-10-05 13:36:27 -0700

    Adding meta files and sample updates.

---
+++ net.j38.websockets.client/Samples~/SendReceive/SendReceive.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using J38.WebSockets.Client;
+
+public class SendReceive : MonoBehaviour
+{
+    string url = "wss://simple-websocket-demo.glitch.me/";
+    private WebSocketClient webSocketClient;
+
+    void Start()
+    {
+        webSocketClient = new WebSocketClient(url);
+
+        webSocketClient.OnConnected += () =>
+        {
+            webSocketClient.Send("ping");
+        };
+
+        webSocketClient.OnReceived += (string message) =>
+        {
+            Debug.Log(message);
+        };
+
+    }
+}
